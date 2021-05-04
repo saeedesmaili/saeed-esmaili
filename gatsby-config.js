9e091b3a7b8a02c75ffb22eefffdbf4aad24a314
Saeed <saaeed.es20@gmail.com>
2021-05-05 03:01:14 +0430

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,12 +54,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `242434028`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
