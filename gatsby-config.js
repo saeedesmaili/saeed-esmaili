beeb8303518c9299d0e6ed6bdd76e658345d4826
Saeed <saaeed.es20@gmail.com>
2021-05-08 18:14:46 +0430

    fix comma bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -187,7 +187,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-advanced-sitemap`,
-    ,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
